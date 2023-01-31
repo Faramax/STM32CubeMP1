1774cac7ff914b9efad8ccf1c176c72c962e1de3
sergeyblagodarnyy <sergey.blagodarnyy@faramax.com>
2023-01-31 15:48:41 +0100

    Added utc-m5.cmake target for open-amp and libmetal

--- Middlewares/Third_Party/OpenAMP/open-amp/cmake/platforms/utc-m4.cmake
+++ Middlewares/Third_Party/OpenAMP/open-amp/cmake/platforms/utc-m4.cmake
@@ -4,8 +4,9 @@ set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER CACHE STRING "")
 set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER CACHE STRING "")
 
 set (WITH_DOC OFF)
+set (WITH_APPS OFF)
 
-set (CMAKE_SYSTEM_PROCESSOR   "arm"      CACHE STRING "")
+set (CMAKE_SYSTEM_PROCESSOR   "arm"              CACHE STRING "")
 set (MACHINE                  "template" CACHE STRING "")
 
 set (CROSS_PREFIX             "/home/sergey/projects/developUTC/source/arm-tools/gcc/bin/arm-none-eabi-" CACHE STRING "")
@@ -20,13 +21,14 @@ set(CMAKE_C_STANDARD 11)
 set(CMAKE_C_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
-set (CMAKE_C_CXX_COMMON_FLAGS  "${CMAKE_C_CXX_COMMON_FLAGS} -Wall -Wextra -Werror -Wno-unused-variable -Wno-unused-parameter -Wno-psabi")
-set (STM32_FLAGS               "${STM32_FLAGS} -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -mabi=aapcs")
-set (CMAKE_C_FLAGS             "${CMAKE_C_FLAGS} ${CMAKE_C_CXX_COMMON_FLAGS} ${STM32_FLAGS}")
-set (CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} -flto --specs=nosys.specs")
+set(CMAKE_C_CXX_COMMON_FLAGS  "${CMAKE_C_CXX_COMMON_FLAGS} -Wall -Wextra -Werror -Wno-unused-variable -Wno-unused-parameter -Wno-psabi -Wno-deprecated-declarations")
+set(STM32_FLAGS               "${STM32_FLAGS} -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -mabi=aapcs")
+set(CMAKE_C_FLAGS             "${CMAKE_C_FLAGS} ${CMAKE_C_CXX_COMMON_FLAGS} ${STM32_FLAGS} -I${CMAKE_CURRENT_SOURCE_DIR}/../libmetal/install/m4-generic-release/usr/local/include")
+set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} -flto --specs=nosys.specs")
+
+set (LIBMETAL_LIB          "{CMAKE_CURRENT_SOURCE_DIR}/../libmetal/install/m4-generic-release/usr/local/lib/"     CACHE STRING "")
+set (LIBMETAL_INCLUDE_DIR  "{CMAKE_CURRENT_SOURCE_DIR}/../libmetal/install/m4-generic-release/usr/local/include"  CACHE STRING "")
 
 include (CMakeForceCompiler)
 CMAKE_FORCE_C_COMPILER   ("${CROSS_PREFIX}gcc" GNU)
 CMAKE_FORCE_CXX_COMPILER ("${CROSS_PREFIX}g++" GNU)
-
-#include (cross-generic-gcc)
